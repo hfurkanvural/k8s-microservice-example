d330b5ab093de7ec9a48bef271ee6336ce5b00ce
hfurkanvural <hfurkanvural@gmail.com>
2020-07-23 19:54:41 +0300

    microservice path update

--- ms1/server.js
+++ ms1/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 8080
 
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => res.send('This is microservice1!'));
 app.get('/api/ms1/hello', (req, res) => res.send('Hello from Node Microservice 1!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
